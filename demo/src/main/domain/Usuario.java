fc67074193af0e28e2db96aad1d1837ad94a7859
ericksilverioSaibWeb <erick.silverio@saibweb.com.br>
2024-03-26 20:39:58 -0300

    Adicionando pacote domain e classe usuário

---
+++ demo/src/main/domain/Usuario.java
@@ -0,0 +1,22 @@
+package main.domain;
+
+public class Usuario {
+    private String nome;
+    private Long id;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
